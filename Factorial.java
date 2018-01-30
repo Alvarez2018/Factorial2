9d97efb7236a5c69b1c7f08c0af295680230d28c
Luis Alvarez <luisalvrez214@gmail.com>
2018-01-29 18:45:54 -0600

    El archivo Tiene ahora un codigo

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial{
+public static void main (String args []){
+int i,factorial=1;
+int numero=5;
+for(i=1;i<=numero;i++)
+{
+factorial=factorial*i;
+}
+System.out.println("EL factorial de " +numero "es" +factorial);
+}
+}
+
